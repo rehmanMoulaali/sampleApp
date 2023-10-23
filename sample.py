38d8c2d7342e05f001398278d9382586b04f4f92
rehmanMoulaali <rehman.arjunagi@irisidea.com>
2023-10-23 14:52:22 +0530

    mad hi api

--- sample.py
+++ sample.py
@@ -3,5 +3,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "hello world";
+
+@app.route("/hi")
+def hi():
+    return "hi to everyone";
+
 if(__name__=='__main__'):
     app.run(host='0.0.0.0')
